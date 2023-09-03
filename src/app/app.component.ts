ab1f5bf09f4fcc1cfdf463b8d05c87f965575ae9
Adrian Erhan <75773955+Rianer@users.noreply.github.com>
2023-09-03 21:42:56 +0300

    Images fade-in fade-out animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+  group,
+  state,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate(1000)]),
+      transition('*=>void', [animate(500)]),
+    ]),
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gallery-project';
+  images: String[] = [
+    'car-img-1.jpg',
+    'city-night.jpg',
+    'corner-building.jpg',
+    'metro-station.jpg',
+    'river-city-img.jpg',
+    'underpass.jpg',
+  ];
+
+  animatedImages: String[] = [];
+
+  ngOnInit(): void {
+    this.animateElements();
+  }
+
+  animateElements() {
+    const delay = 100; //ms
+    this.images.forEach((element, index) => {
+      setTimeout(() => {
+        this.animatedImages.push(element);
+      }, delay * index);
+    });
+  }
+
+  removeElement(element: String) {
+    const index = this.images.indexOf(element);
+    if (index !== -1) {
+      this.images.splice(index, 1);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ImageGalleryComponent, NavigationBarComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
